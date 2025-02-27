4aff76589654b1133d3b336ad6f5bd487382060d
root <root@test-------onion.asia-east2-c.c.glossy-window-275608.internal>
2025-02-27 05:57:48 +0000

    support union

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# AeroNyxDecentralizedNodeOnionTool
+# AeroNyxTools
+Tools for deploying AeroNyxNode services.
+
+# Deploy
+## Download source
+```shell 
+git clone https://github.com/AeroNyxNetwork/AeroNyxDecentralizedNodeOnionTool.git
+cd AeroNyxDecentralizedNodeOnionTool/
+```
+
+## Deploy Environment
+```shell
+python3 main.py -I
+```
+
+## Register node
+```shell
+python3 main.py --set_register --owner=Your client public key --name=node_name
+```
+- owner--Owner of node (You can copy the pubkey from the AeroNyx client as the owner of the node)
+- name--Name of node
+
+## Run service
+```sheel
+python3 main.py --start
+```
+
+# More
+```
+-h, --help     show this help message and exit
+-I, --install  install dependent environment
+-U, --update   update all
+-p, --private  show private key
+-P, --public   show public key
+--start        start the service
+--stop         stop the service
+-v, --version  show program's version number and exit
+```
---
+++ main.py
@@ -0,0 +1,145 @@
+import argparse
+import os
+import json
+# decode args
+def ArgInit():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-I', '--install', action='store_const', dest='install',
+        const='1',
+        help='install dependent environment')
+    parser.add_argument('-U', '--update', action='store_const', dest='update',
+        const='1',
+        help='update all')
+    parser.add_argument('-p', '--private', action='store_const', dest='private',
+        const='1',
+        help='show private key')
+    parser.add_argument('-P', '--public', action='store_const', dest='public',
+        const='1',
+        help='show public key')
+    parser.add_argument('--start', action='store_const', dest='start',
+        const='1',
+        help='start the service')
+    parser.add_argument('--stop', action='store_const', dest='stop',
+        const='1',
+        help='stop the service')
+    
+    # version, owner, port, name, country
+    parser.add_argument('--set_register', action='store_const', dest='set_register',
+        const='1',
+        help='register node info')
+
+    parser.add_argument('--owner', action='store', dest='owner',
+        help='setup owner of node')
+
+    parser.add_argument('--port', action='store', dest='port',
+        help='setup port of node')
+
+    parser.add_argument('--name', action='store', dest='name',
+        help='setup name of node')
+
+    parser.add_argument('--country', action='store', dest='country',
+        help='setup owncountryer of node')
+        
+    parser.add_argument('--register', action='store_const', dest='register',
+        const='1',
+        help='show register node info')
+
+    parser.add_argument('-v', '--version', action='version', version='v0.1.1')
+
+    results = parser.parse_args()
+    
+    if results.install != None:
+        install()
+    if results.update != None:
+        update()
+    if results.private != None:
+        private()
+    if results.public != None:
+        public()
+    if results.start != None:
+        start()
+    if results.stop != None:
+        stop()
+    if results.set_register != None:
+        set_register("0.0.1", results.owner, results.port, results.name, results.country)
+    if results.register != None:
+        register()
+
+
+# color output 
+def red_str(str):
+    return "\033[1;31m"+str+"\033[0m"
+def green_str(str):
+    return "\033[1;32m"+str+"\033[0m"
+def check_su():
+    res = os.geteuid()
+    if res != 0:
+        print(red_str("This command must be used with root privileges"))
+        exit()
+
+def install():
+    print("Installing...")
+    check_su()
+    os.system("curl -fsSL https://test.docker.com -o test-docker.sh")
+    os.system("sh test-docker.sh")
+    os.system("apt update")
+    os.system("apt install docker-compose -y")
+    os.system("apt install git -y")
+    os.system("git clone https://github.com/AeroNyxNetwork/NodeDocker.git /opt/node_docker")
+    os.system("chmod a+rw /opt/node_docker/data/aero_nyx_db/logs")
+    os.system("docker-compose -f /opt/node_docker/docker-compose.yml pull")
+    print("Result:"+green_str("success"))
+    
+def update():
+    print("update....")
+    check_su()
+    stop()
+    os.system("git --git-dir=/opt/node_docker/.git --work-tree=/opt/node_docker pull")
+    os.system("docker-compose -f /opt/node_docker/docker-compose.yml pull")
+    start()
+    #  
+    
+def private():
+    r = os.popen("docker exec aero_nyx_node cat /opt/server_config.json")
+    obj = json.loads(r.read())
+    print("private key:0x" + obj["pri_key"])
+    
+def public():
+    r = os.popen("docker exec aero_nyx_node cat /opt/server_config.json")
+    obj = json.loads(r.read())
+    print("public key:0x" + obj["pub_key"])
+    
+def start():
+    print("starting...")
+    os.system("docker-compose -f /opt/node_docker/docker-compose.yml  up -d")
+    
+def stop():
+    print("stopping...")
+    os.system("docker-compose -f /opt/node_docker/docker-compose.yml  down")
+
+def set_register(version, owner, port, name, country):
+    country_list = ["AD","AE","AF","AG","AI","AL","AM","AO","AR","AT","AU","AZ","BB","BD","BE","BF","BG","BH","BI","BJ","BL","BM","BN","BO","BR","BS","BW","BY","BZ","CA","CF","CG","CH","CK","CL","CM","CN","CO","CR","CS","CU","CY","CZ","DE","DJ","DK","DO","DZ","EC","EE","EG","ES","ET","FI","FJ","FR","GA","GB","GD","GE","GF","GH","GI","GM","GN","GR","GT","GU","GY","HK","HN","HT","HU","ID","IE","IL","IN","IQ","IR","IS","IT","JM","JO","JP","KE","KG","KH","KP","KR","KT","KW","KZ","LA","LB","LC","LI","LK","LR","LS","LT","LU","LV","LY","MA","MC","MD","MG","ML","MM","MN","MO","MS","MT","MU","MV","MW","MX","MY","MZ","NA","NE","NG","NI","NL","NO","NP","NR","NZ","OM","PA","PE","PF","PG","PH","PK","PL","PR","PT","PY","QA","RO","RU","SA","SB","SC","SD","SE","SG","SI","SK","SL","SM","SN","SO","SR","ST","SV","SY","SZ","TD","TG","TH","TJ","TM","TN","TO","TR","TT","TW","TZ","UA","UG","US","UY","UZ","VC","VE","VN","YE","YU","ZA","ZM","ZR","ZW"]
+    file_path = "/opt/node_docker/data/aero_nyx_sync/config.json"
+    json_file = open(file_path, "w")
+    json_obj = {
+        "version": version, 
+        "owner": owner,
+        "name": name
+    }
+    if version == None or owner == None or name == None:
+        print("set register faild, must set owner,name in same time.")
+    else:
+        json.dump(json_obj, json_file)
+        print("set register success")
+        
+def register() :
+    r = os.popen("docker exec aero_nyx_sync cat /opt/aero_nyx_sync/config.json")
+    obj = json.loads(r.read())
+    print(obj)
+
+#python3 main.py --set_register --owner=0432215ed9e25b8fc3e38a6df609ce46995b418b99082d367d7999c07496373b1f5e6ee09375c4f7e423f5a53cb7fc9e75f4cf8a13c0106799a9be01f759f264a6 --port=1011 --name=USNAME --country=US
+if __name__ == "__main__":
+    ArgInit()
+
+
+
